79aec5670e94b75119644b3d9f2351820d682e15
Motasim Ali Bhatti <bitf13a011@pucit.edu.pk>
2020-06-18 11:38:19 +0500

    Implementing and using a Node Module of Database Operations

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dpoper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -8,21 +9,19 @@ MongoClient.connect(url,(err,client)=>{
     assert.equal(err,null);
     console.log('Connected successfully to mongodb');
     const db = client.db(dbname);
-    const collection = db.collection('dishes');
-    collection.insertOne({"name":"Pepperoni Pizaa", "discription":"Test 2"},(err,result)=>{
-        assert.equal(err,null);
-        console.log('After insert: \n');
-        console.log(result.ops);
-
-        collection.find({}).toArray((err,docs)=>{
-            assert.equal(err,null);
-            console.log('Found:\n');
-            console.log(docs);
-
-            db.dropCollection('dishes',(err,result)=>{
-                assert.equal(err,null);
-                client.close();
-                console.log('Droped dishes collection and closed the connection');
+    dpoper.insertDocument(db, {name : "Vodonut", description: 'Test'}, 'dishes', (result)=>{
+        console.log('Insert Document:\n', result.ops);
+        dpoper.findDocuments(db,'dishes',(docs)=>{
+            console.log('Found Documents:\n', docs);
+            dpoper.updateDocument(db, {name : "Vodonut"},{description: "update test"}, 'dishes', (result)=>{
+                console.log('Update Document:\n', result.result);
+                dpoper.findDocuments(db,'dishes',(docs)=>{
+                    console.log('Find Documents:\n', docs);
+                    db.dropCollection('dishes',(result)=>{
+                        console.log('Dropped Collection: ', result);
+                        client.close();
+                    })
+                })
             })
         })
     });
